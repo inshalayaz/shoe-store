9491028aecd9843b40f2bc995c351337d257800e
inshal <inshal.ayaz14@gmail.com>
2022-02-17 17:45:30 +0500

    images import method changed

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,49 +1,57 @@
+import img1 from '../../images/1.jpg';
+import img2 from '../../images/2.jpg';
+import img3 from '../../images/3.jpg';
+import img4 from '../../images/4.jpg';
+import img5 from '../../images/5.jpg';
+import img6 from '../../images/6.jpg';
+import img7 from '../../images/7.jpg';
+import img8 from '../../images/8.jpg';
 const data = [
   {
     slug: '1',
-    img: '/src/images/1.jpg',
+    img: img1,
     brand: 'AIR JORDAN',
     title: 'AIR JORDAN 5 RETRO "RACER BLUE"',
   },
   {
     slug: '2',
-    img: '/src/images/2.jpg',
+    img: img2,
     brand: 'Nike',
     title: 'UNION LA X DUNK LOW "PASSPORT PACK - PISTACHIO"',
   },
   {
     slug: '3',
-    img: '/src/images/3.jpg',
+    img: img3,
     brand: 'AIR JORDAN',
     title: 'WMNS AIR JORDAN 5 RETRO LOW "GIRLS THAT HOOP"',
   },
   {
     slug: '4',
-    img: '/src/images/4.jpg',
+    img: img4,
     brand: 'Nike',
     title: 'AIR JORDAN 1 MID SE "DIAMOND"',
   },
   {
     slug: '5',
-    img: '/src/images/5.jpg',
+    img: img5,
     brand: 'Nike',
     title: 'ACRONYM X BLAZER LOW "NIGHT MAROON"',
   },
   {
     slug: '6',
-    img: '/src/images/6.jpg',
+    img: img6,
     brand: 'Nike',
     title: 'AIR TRAINER 1 "CORIANDER"',
   },
   {
     slug: '7',
-    img: '/src/images/7.jpg',
+    img: img7,
     brand: 'Nike',
     title: 'ACRONYM X BLAZER LOW "BLACK OLIVE AURA"',
   },
   {
     slug: '8',
-    img: '/src/images/8.jpg',
+    img: img8,
     brand: 'adidas',
     title: 'BAD BUNNY X FORUM POWERPHASE "CATCH AND THRO…"',
   },
